c114915c079c4a56db90e1907e9f784057a3da0a
Mashite <masite_bayram@hotmail.com>
2022-02-14 14:26:14 +0300

    ok

---
+++ TestIdentity/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestIdentity.Identity;
+using TestIdentity.Models;
+
+namespace TestIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public AccountController()
+        {
+            userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new IdentityDataContext()));
+        }
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = register.Username, Email = register.Email };
+                var result = userManager.Create(user, register.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(register);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
